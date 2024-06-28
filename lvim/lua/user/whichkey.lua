fff90e5dc41c865b4a64a24f494de3c0495f8450
isseii10 <t1l.g2.o11g@gmail.com>
2024-06-28 20:06:04 +0900

    fix

--- lvim/lua/user/whichkey.lua
+++ lvim/lua/user/whichkey.lua
@@ -1,16 +1,16 @@
 -- replace <leader>f
 lvim.builtin.which_key.mappings["f"] = {
-      name = "+Find",
-      b = { "<cmd>Telescope current_buffer_fuzzy_find<CR>", "Current buffer fuzzy-find" },
-      f = {
-            function()
-                  require("lvim.core.telescope.custom-finders").find_project_files { previewer = false }
-            end,
-            "Find Git Files",
-      },
-      F = { "<cmd>Telescope find_files<CR>", "Find files" },
-      g = { "<cmd>Telescope live_grep<CR>", "Live grep" },
-      h = { "<cmd>Telescope help_tags<CR>", "help tags" },
-      m = { "<cmd>Telescope marks<CR>", "Marks" },
-      r = { "<cmd>Telescope oldfiles<CR>", "Find recent files" },
+  name = "+Find",
+  b = { "<cmd>Telescope current_buffer_fuzzy_find<CR>", "Current buffer fuzzy-find" },
+  f = {
+    function()
+      require("lvim.core.telescope.custom-finders").find_project_files { previewer = false }
+    end,
+    "Find Git Files",
+  },
+  F = { "<cmd>Telescope find_files<CR>", "Find files" },
+  g = { "<cmd>Telescope live_grep<CR>", "Live grep" },
+  h = { "<cmd>Telescope help_tags<CR>", "help tags" },
+  m = { "<cmd>Telescope marks<CR>", "Marks" },
+  r = { "<cmd>Telescope oldfiles<CR>", "Find recent files" },
 }

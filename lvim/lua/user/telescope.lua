a1c208de8ba6542ee67436b94512caa8a793bbba
isseii10 <t1l.g2.o11g@gmail.com>
2024-06-29 23:54:21 +0900

    trouble

--- lvim/lua/user/telescope.lua
+++ lvim/lua/user/telescope.lua
@@ -1,6 +1,8 @@
 local _, actions = pcall(require, "telescope.actions")
 local _, builtin = pcall(require, "telescope.builtin")
 
+local open_with_trouble = require("trouble.sources.telescope").open
+
 lvim.builtin.telescope.defaults.mappings = {
   i = {
     ["<C-c>"] = actions.close,
@@ -11,6 +13,7 @@ lvim.builtin.telescope.defaults.mappings = {
     ["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
     ["<CR>"] = actions.select_default + actions.center,
     -- ["<C-i>"] = my_cool_custom_action,
+    ["<C-t>"] = open_with_trouble,
   },
   n = {
     ["<C-c>"] = actions.close,
@@ -19,5 +22,6 @@ lvim.builtin.telescope.defaults.mappings = {
     ["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
     -- ["<C-Space>"] = custom_actions.fuzzy_filter_results,
     -- ["<C-i>"] = my_cool_custom_action,
+    ["<C-t>"] = open_with_trouble,
   },
 }

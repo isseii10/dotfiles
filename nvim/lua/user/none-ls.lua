88f7179000d3f8e2ab8c0efed16049787098a7f8
isseii10 <t1l.g2.o11g@gmail.com>
2024-07-18 04:11:34 +0900

    fix

--- nvim/lua/user/none-ls.lua
+++ nvim/lua/user/none-ls.lua
@@ -16,14 +16,12 @@ function M.config()
     sources = {
       formatting.stylua,
       formatting.prettier,
-      formatting.black,
+      formatting.gofmt,
       -- formatting.prettier.with {
       --   extra_filetypes = { "toml" },
       --   -- extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
       -- },
       -- formatting.eslint,
-      null_ls.builtins.diagnostics.flake8,
-      -- diagnostics.flake8,
       null_ls.builtins.completion.spell,
     },
   }

477c0230d2905a2ff1290fa8d5a53db162fa6b9d
isseii10 <t1l.g2.o11g@gmail.com>
2024-07-18 04:11:51 +0900

    tmp

--- nvim/lua/user/telescope.lua
+++ nvim/lua/user/telescope.lua
@@ -7,11 +7,11 @@ function M.config()
   local wk = require "which-key"
   wk.add {
     { "<leader>bb", "<cmd>Telescope buffers previewer=false<cr>",                       desc = "Find" },
-    { "<leader>fb", "<cmd>Telescope git_branches<cr>",                                  desc = "Checkout branch" },
+    { "<leader>fb", "<cmd>Telescope current_buffer_fuzzy_find<CR>",                     desc = "current buffer" },
     { "<leader>fc", "<cmd>Telescope colorscheme<cr>",                                   desc = "Colorscheme" },
     { "<leader>ff", "<cmd>Telescope find_files<cr>",                                    desc = "Find files" },
     { "<leader>fp", "<cmd>lua require('telescope').extensions.projects.projects()<cr>", desc = "Projects" },
-    { "<leader>ft", "<cmd>Telescope live_grep<cr>",                                     desc = "Find Text" },
+    { "<leader>fg", "<cmd>Telescope live_grep<cr>",                                     desc = "Live grep" },
     { "<leader>fh", "<cmd>Telescope help_tags<cr>",                                     desc = "Help" },
     { "<leader>fl", "<cmd>Telescope resume<cr>",                                        desc = "Last Search" },
     { "<leader>fr", "<cmd>Telescope oldfiles<cr>",                                      desc = "Recent File" },
@@ -69,6 +69,10 @@ function M.config()
       find_files = {
         theme = "dropdown",
         previewer = false,
+      },
+
+      current_buffer_fuzzy_find = {
+        theme = "dropdown",
       },
 
       buffers = {

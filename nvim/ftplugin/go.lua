e8eacf4cc050e6ab2eae5a1f23bafed2846eb640
isseii10 <t1l.g2.o11g@gmail.com>
2024-07-20 04:01:59 +0900

    fix

--- nvim/ftplugin/go.lua
+++ nvim/ftplugin/go.lua
@@ -2,7 +2,7 @@ local status_ok, wk = pcall(require, "which-key")
 if status_ok then
   wk.add {
     { "<leader>c",  group = "Go" },
-    { "<leader>ca", "<cmd>GoAddTest", desc = "add test" },
-    { "<leader>cA", "<cmd>GoAddAllTest", desc = "add all tests" },
+    { "<leader>ca", "<cmd>GoAddTest<cr>", desc = "add test" },
+    { "<leader>cA", "<cmd>GoAddAllTest<cr>", desc = "add all tests" },
   }
 end

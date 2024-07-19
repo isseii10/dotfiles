ad26d5a76bd4249a79ff2baacebfbcd419333d81
issei.terada <issei.terada@showcase-gig.com>
2024-07-19 15:00:40 +0900

    add todo comment

---
+++ nvim/lua/user/todo-comments.lua
@@ -0,0 +1,25 @@
+local M = {
+  "folke/todo-comments.nvim",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  event = "VeryLazy",
+}
+
+function M.config()
+  vim.keymap.set("n", "]t", function()
+    require("todo-comments").jump_next()
+  end, { desc = "Next todo comment" })
+
+  vim.keymap.set("n", "[t", function()
+    require("todo-comments").jump_prev()
+  end, { desc = "Previous todo comment" })
+
+
+  require("todo-comments").setup {}
+end
+
+return M
+
+-- TODO: a
+-- WARN: b
+-- FIXME: b
+

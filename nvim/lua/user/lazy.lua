a91baaa9f89189085ee5737236038e159f4d5b79
isseii10 <t1l.g2.o11g@gmail.com>
2024-07-19 21:06:15 +0900

    fix

--- nvim/lua/user/lazy.lua
+++ nvim/lua/user/lazy.lua
@@ -14,7 +14,7 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup {
   spec = LAZY_PLUGIN_SPEC,
   install = {
-    colorscheme = { "default" },
+    colorscheme = { "onenord" },
   },
   ui = {
     border = "rounded",

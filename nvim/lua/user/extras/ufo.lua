fd68c4e2096007e055931992b78f54446ed572ce
isseii10 <t1l.g2.o11g@gmail.com>
2024-07-21 22:16:00 +0900

    event BufRead

--- nvim/lua/user/extras/ufo.lua
+++ nvim/lua/user/extras/ufo.lua
@@ -4,7 +4,10 @@ local M = {
     "kevinhwang91/promise-async",
     "luukvbaal/statuscol.nvim",
   },
-  event = { "BufReadPre", "BufNewFile" },
+  -- don't use "BufReadPre"
+  -- use "BufRead" or "VeryLazy" instead.
+  -- https://github.com/kevinhwang91/nvim-ufo/issues/47
+  event = { "BufRead" },
 }
 
 function M.config()
@@ -29,6 +32,34 @@ function M.config()
   vim.o.foldenable = true
   vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
 
+  -- local handler = function(virtText, lnum, endLnum, width, truncate)
+  --   local newVirtText = {}
+  --   local suffix = (" 󰡏 %d "):format(endLnum - lnum)
+  --   local sufWidth = vim.fn.strdisplaywidth(suffix)
+  --   local targetWidth = width - sufWidth
+  --   local curWidth = 0
+  --   for _, chunk in ipairs(virtText) do
+  --     local chunkText = chunk[1]
+  --     local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+  --     if targetWidth > curWidth + chunkWidth then
+  --       table.insert(newVirtText, chunk)
+  --     else
+  --       chunkText = truncate(chunkText, targetWidth - curWidth)
+  --       local hlGroup = chunk[2]
+  --       table.insert(newVirtText, { chunkText, hlGroup })
+  --       chunkWidth = vim.fn.strdisplaywidth(chunkText)
+  --       -- str width returned from truncate() may less than 2nd argument, need padding
+  --       if curWidth + chunkWidth < targetWidth then
+  --         suffix = suffix .. (" "):rep(targetWidth - curWidth - chunkWidth)
+  --       end
+  --       break
+  --     end
+  --     curWidth = curWidth + chunkWidth
+  --   end
+  --   table.insert(newVirtText, { suffix, "MoreMsg" })
+  --   return newVirtText
+  -- end
+
   local handler = function(virtText, lnum, endLnum, width, truncate)
     local newVirtText = {}
     local suffix = (" 󰡏 %d "):format(endLnum - lnum)
@@ -36,22 +67,25 @@ function M.config()
     local targetWidth = width - sufWidth
     local curWidth = 0
     for _, chunk in ipairs(virtText) do
-      local chunkText = chunk[1]
-      local chunkWidth = vim.fn.strdisplaywidth(chunkText)
-      if targetWidth > curWidth + chunkWidth then
-        table.insert(newVirtText, chunk)
-      else
-        chunkText = truncate(chunkText, targetWidth - curWidth)
-        local hlGroup = chunk[2]
-        table.insert(newVirtText, { chunkText, hlGroup })
-        chunkWidth = vim.fn.strdisplaywidth(chunkText)
-        -- str width returned from truncate() may less than 2nd argument, need padding
-        if curWidth + chunkWidth < targetWidth then
-          suffix = suffix .. (" "):rep(targetWidth - curWidth - chunkWidth)
+      -- Debug information
+      print("Processing chunk:", vim.inspect(chunk))
+      if type(chunk) == "table" and #chunk >= 2 then
+        local chunkText = chunk[1]
+        local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+        if targetWidth > curWidth + chunkWidth then
+          table.insert(newVirtText, chunk)
+        else
+          chunkText = truncate(chunkText, targetWidth - curWidth)
+          local hlGroup = chunk[2]
+          table.insert(newVirtText, { chunkText, hlGroup })
+          chunkWidth = vim.fn.strdisplaywidth(chunkText)
+          if curWidth + chunkWidth < targetWidth then
+            suffix = suffix .. (" "):rep(targetWidth - curWidth - chunkWidth)
+          end
+          break
         end
-        break
+        curWidth = curWidth + chunkWidth
       end
-      curWidth = curWidth + chunkWidth
     end
     table.insert(newVirtText, { suffix, "MoreMsg" })
     return newVirtText
@@ -70,8 +104,8 @@ function M.config()
     provider_selector = function(bufnr, filetype, buftype)
       -- if you prefer treesitter provider rather than lsp,
       -- return ftMap[filetype] or {'treesitter', 'indent'}
-      return ftMap[filetype]
-      -- return { "treesitter", "indent" }
+      -- return ftMap[filetype]
+      return { "treesitter", "indent" }
 
       -- refer to ./doc/example.lua for detail
     end,
@@ -91,8 +125,8 @@ function M.config()
     },
   }
 
-  vim.keymap.set("n", "zR", require("ufo").openAllFolds, {desc = "open all folds"})
-  vim.keymap.set("n", "zM", require("ufo").closeAllFolds, {desc = "close all folds"})
+  vim.keymap.set("n", "zR", require("ufo").openAllFolds, { desc = "open all folds" })
+  vim.keymap.set("n", "zM", require("ufo").closeAllFolds, { desc = "close all folds" })
   vim.keymap.set("n", "zr", require("ufo").openFoldsExceptKinds)
   vim.keymap.set("n", "zm", require("ufo").closeFoldsWith) -- closeAllFolds == closeFoldsWith(0)
   vim.keymap.set("n", "K", function()

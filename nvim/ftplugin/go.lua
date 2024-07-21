db9c7ca2af4ddf2997c1e14c831ade426b209a97
isseii10 <t1l.g2.o11g@gmail.com>
2024-07-21 18:00:00 +0900

    iferr

--- nvim/ftplugin/go.lua
+++ nvim/ftplugin/go.lua
@@ -5,5 +5,6 @@ if status_ok then
     { "<leader>ca", "<cmd>GoAddTest<cr>", desc = "add test" },
     { "<leader>cA", "<cmd>GoAddAllTest<cr>", desc = "add all tests" },
     { "<leader>ct", "<cmd>GoModTidy<cr>", desc = "go mod tidy" },
+    { "<leader>ce", "<cmd>GoIfErr<cr>", desc = "if err" },
   }
 end

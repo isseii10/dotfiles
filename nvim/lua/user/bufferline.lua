469f19efa82c67b00e0a5fc26867ffd16d68d237
issei.terada <issei.terada@showcase-gig.com>
2024-07-22 10:47:09 +0900

    fixme

--- nvim/lua/user/bufferline.lua
+++ nvim/lua/user/bufferline.lua
@@ -16,34 +16,34 @@ M.config = function()
     { "<leader>br", "<cmd>BufferLineCloseRight<cr>", desc = "Close buffers to the right" },
   }
 
-  vim.api.nvim_set_hl(0, "MyBufferSelected", { fg = "#ffffff", bold = true })
-  vim.api.nvim_set_hl(0, "MyHarpoonSelected", { fg = "#ffffff", bold = true })
+  -- vim.api.nvim_set_hl(0, "MyBufferSelected", { fg = "#ffffff", bold = true })
+  -- vim.api.nvim_set_hl(0, "MyHarpoonSelected", { fg = "#ffffff", bold = true })
 
   require("bufferline").setup {
     options = {
-      custom_areas = {
-        left = function()
-          local result = {}
-          local items = require("harpoon"):list().items
-          local icons = require "user.icons"
-          for i = 1, #items do
-            local fname = items[i].value
-            local fullpath = vim.fn.fnamemodify(fname, ":p")
-            local name = " " .. icons.ui.BookMark .. i .. " " .. vim.fn.fnamemodify(fname, ":t") .. " "
-            local activename = " " .. icons.ui.BookMarkFill .. i .. " " .. vim.fn.fnamemodify(fname, ":t") .. " "
-            if fullpath == vim.fn.expand "%:p" then
-              table.insert(result, { text = icons.ui.BoldLineMiddle, link = 'BufferLineIndicatorSelected' })
-              -- table.insert(result, { text = name, link = 'MyHarpoonSelected' })
-              table.insert(result, { text = activename, link = "MyHarpoonSelected" })
-            else
-              -- print('inactive: ' .. name)
-              table.insert(result, { text = icons.ui.LineMiddle })
-              table.insert(result, { text = name, link = "BufferLineBufferVisible" })
-            end
-          end
-          return result
-        end,
-      },
+      -- custom_areas = {
+      --   left = function()
+      --     local result = {}
+      --     local items = require("harpoon"):list().items
+      --     local icons = require "user.icons"
+      --     for i = 1, #items do
+      --       local fname = items[i].value
+      --       local fullpath = vim.fn.fnamemodify(fname, ":p")
+      --       local name = " " .. icons.ui.BookMark .. i .. " " .. vim.fn.fnamemodify(fname, ":t") .. " "
+      --       local activename = " " .. icons.ui.BookMarkFill .. i .. " " .. vim.fn.fnamemodify(fname, ":t") .. " "
+      --       if fullpath == vim.fn.expand "%:p" then
+      --         table.insert(result, { text = icons.ui.BoldLineMiddle, link = 'BufferLineIndicatorSelected' })
+      --         -- table.insert(result, { text = name, link = 'MyHarpoonSelected' })
+      --         table.insert(result, { text = activename, link = "MyHarpoonSelected" })
+      --       else
+      --         -- print('inactive: ' .. name)
+      --         table.insert(result, { text = icons.ui.LineMiddle })
+      --         table.insert(result, { text = name, link = "BufferLineBufferVisible" })
+      --       end
+      --     end
+      --     return result
+      --   end,
+      -- },
       offsets = {
         {
           filetype = "NvimTree",
@@ -53,18 +53,6 @@ M.config = function()
         },
       },
       diagnostics = "nvim_lsp",
-
-      -- options = {
-      --   offsets = {
-      --     {
-      --       filetype = "NvimTree",
-      --       text = "Nvim Tree",
-      --       separator = true,
-      --       text_align = "left",
-      --     },
-      --     diagnostics = "nvim_lsp",
-      --   },
-      -- },
     },
   }
 end

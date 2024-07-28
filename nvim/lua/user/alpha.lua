1aeb91d7c204336bd3f20dd70052cf2e450e7529
isseii10 <t1l.g2.o11g@gmail.com>
2024-07-29 00:35:12 +0900

    alpha

--- nvim/lua/user/alpha.lua
+++ nvim/lua/user/alpha.lua
@@ -8,19 +8,46 @@ local M = {
 function M.config()
   local alpha = require "alpha"
   local dashboard = require "alpha.themes.dashboard"
+  local icons = require "user.icons"
   dashboard.section.header.val = {
-    [[                               __                ]],
-    [[  ___     ___    ___   __  __ /\_\    ___ ___    ]],
-    [[ / _ `\  / __`\ / __`\/\ \/\ \\/\ \  / __` __`\  ]],
-    [[/\ \/\ \/\  __//\ \_\ \ \ \_/ |\ \ \/\ \/\ \/\ \ ]],
-    [[\ \_\ \_\ \____\ \____/\ \___/  \ \_\ \_\ \_\ \_\]],
-    [[ \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
+    -- [[                               __                ]],
+    -- [[  ___     ___    ___   __  __ /\_\    ___ ___    ]],
+    -- [[ / _ `\  / __`\ / __`\/\ \/\ \\/\ \  / __` __`\  ]],
+    -- [[/\ \/\ \/\  __//\ \_\ \ \ \_/ |\ \ \/\ \/\ \/\ \ ]],
+    -- [[\ \_\ \_\ \____\ \____/\ \___/  \ \_\ \_\ \_\ \_\]],
+    -- [[ \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
+
+    [[                                             ]],
+    [[                                             ]],
+    [[                                             ]],
+    [[                                             ]],
+    [[                                             ]],
+    [[                                             ]],
+    [[                                             ]],
+    [[                                             ]],
+    [[                                             ]],
+    [[                                             ]],
+    [[ ▄▄    ▄ ▄▄▄▄▄▄▄ ▄▄▄▄▄▄▄ ▄▄   ▄▄ ▄▄▄ ▄▄   ▄▄ ]],
+    [[█  █  █ █       █       █  █ █  █   █  █▄█  █]],
+    [[█   █▄█ █    ▄▄▄█   ▄   █  █▄█  █   █       █]],
+    [[█       █   █▄▄▄█  █ █  █       █   █       █]],
+    [[█  ▄    █    ▄▄▄█  █▄█  █       █   █       █]],
+    [[█ █ █   █   █▄▄▄█       ██     ██   █ ██▄██ █]],
+    [[█▄█  █▄▄█▄▄▄▄▄▄▄█▄▄▄▄▄▄▄█ █▄▄▄█ █▄▄▄█▄█   █▄█]],
+    [[                                             ]],
+    [[                                             ]],
+    [[                                             ]],
+    [[                                             ]],
+    [[                                             ]],
   }
   dashboard.section.buttons.val = {
-    dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
-    dashboard.button("<space>ff", "  Find file", "<cmd>Telescope find_files<CR>"),
-    dashboard.button("<space>fr", "  Recently opend files", "<cmd>Telescope oldfiles<CR>"),
-    dashboard.button("<ctrl>p", "  Recent projects", ":lua require('telescope').extensions.projects.projects()<CR>"),
+    dashboard.button("SPC ff", icons.ui.FindFile .. " Find file", "<cmd>Telescope find_files<CR>"),
+    dashboard.button("SPC fr", icons.ui.History .. " Recently opend files", "<cmd>Telescope oldfiles<CR>"),
+    dashboard.button(
+      "CTL p",
+      icons.ui.Project .. " Recent projects",
+      ":lua require('telescope').extensions.projects.projects()<CR>"
+    ),
 
     dashboard.button("q", "󰅚  Quit NVIM", ":qa<CR>"),
   }

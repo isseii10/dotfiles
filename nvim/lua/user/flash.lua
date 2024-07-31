2453d90a48e884193eb9b44b5a71c62330a1e4c3
isseii10 <t1l.g2.o11g@gmail.com>
2024-08-01 02:23:58 +0900

    fix flash

--- nvim/lua/user/flash.lua
+++ nvim/lua/user/flash.lua
@@ -1,14 +1,33 @@
-return {
+local M ={
   "folke/flash.nvim",
   event = "VeryLazy",
   ---@type Flash.Config
   opts = {},
   -- stylua: ignore
   keys = {
-    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-    { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-    { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-    { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-    { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    { "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,       desc = "Flash" },
+    { "S",     mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    -- { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+    -- { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+    { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,     desc = "Toggle Flash Search" },
+  },
+  highlight = {
+    -- show a backdrop with hl FlashBackdrop
+    backdrop = true,
+    -- Highlight the search matches
+    matches = true,
+    -- extmark priority
+    priority = 5000,
+    groups = {
+      match = "FlashMatch",
+      current = "FlashCurrent",
+      backdrop = "FlashBackdrop",
+      label = "FlashLabel",
+    },
   },
 }
+
+vim.api.nvim_set_hl(0, "Flashlabel", { fg = "#ed6d35", bold = true, underline = true })
+vim.api.nvim_set_hl(0, "FlashMatch", { fg = "#ffae8a", underline = false })
+
+return M

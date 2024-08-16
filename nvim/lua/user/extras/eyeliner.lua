0acb23f4d5232f08a4bf333c30e9f6b99a1c0e8e
isseii10 <t1l.g2.o11g@gmail.com>
2024-08-17 03:26:15 +0900

    eyeliner

--- nvim/lua/user/extras/eyeliner.lua
+++ nvim/lua/user/extras/eyeliner.lua
@@ -3,12 +3,13 @@ local M = {
   event = { "BufReadPre", "BufNewFile" },
 }
 
-vim.api.nvim_set_hl(0, 'EyelinerPrimary', { fg = '#ed6d35', bold = true, underline = true })
-vim.api.nvim_set_hl(0, 'EyelinerSecondary', { fg = '#ffae8a', underline = true })
+vim.api.nvim_set_hl(0, "EyelinerPrimary", { fg = "#ed6d35", bold = true, underline = true })
+vim.api.nvim_set_hl(0, "EyelinerSecondary", { fg = "#ffae8a", underline = false })
 
 function M.config()
   require("eyeliner").setup {
     highlight_on_key = true,
+    dim = true,
   }
 end
 

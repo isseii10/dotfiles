cb93b022e2a6e0143c1170c6635ad9d51afdcff5
isseii10 <t1l.g2.o11g@gmail.com>
2024-08-17 03:40:36 +0900

    treesj event

--- nvim/lua/user/extras/treesj.lua
+++ nvim/lua/user/extras/treesj.lua
@@ -1,8 +1,12 @@
-return {
-  'Wansmer/treesj',
-  keys = { '<space>m', '<space>j', '<space>s' },
-  dependencies = { 'nvim-treesitter/nvim-treesitter' }, -- if you install parsers with `nvim-treesitter`
-  config = function()
-    require('treesj').setup({--[[ your config ]]})
-  end,
+local M = {
+  "Wansmer/treesj",
+  -- keys = { "<space>m", "<space>j", "<space>s" },
+  event = "BufEnter",
+  dependencies = { "nvim-treesitter/nvim-treesitter" }, -- if you install parsers with `nvim-treesitter`
 }
+
+function M.config()
+  require("treesj").setup {}
+end
+
+return M

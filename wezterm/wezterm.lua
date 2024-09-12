363bbeb24a262b7d32c8c209e34764fc6d1e889f
isseii10 <t1l.g2.o11g@gmail.com>
2024-09-12 23:24:05 +0900

    smart-splits

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,6 +1,6 @@
 local wezterm = require("wezterm")
-local config = {}
 
+local config = {}
 if wezterm.config_builder then
   config = wezterm.config_builder()
 end
@@ -43,5 +43,50 @@ config.disable_default_key_bindings = true
 local keybind = require("keybinds")
 config.keys = keybind.keys
 config.key_tables = keybind.key_tables
+
+-- if you are *NOT* lazy-loading smart-splits.nvim (recommended)
+local function is_vim(pane)
+  -- this is set by the plugin, and unset on ExitPre in Neovim
+  return pane:get_user_vars().IS_NVIM == "true"
+end
+
+local direction_keys = {
+  h = "Left",
+  j = "Down",
+  k = "Up",
+  l = "Right",
+}
+
+local function split_nav(resize_or_move, key)
+  return {
+    key = key,
+    mods = resize_or_move == "resize" and "META" or "CTRL",
+    action = wezterm.action_callback(function(win, pane)
+      if is_vim(pane) then
+        -- pass the keys through to vim/nvim
+        win:perform_action({
+          SendKey = { key = key, mods = resize_or_move == "resize" and "META" or "CTRL" },
+        }, pane)
+      else
+        if resize_or_move == "resize" then
+          win:perform_action({ AdjustPaneSize = { direction_keys[key], 3 } }, pane)
+        else
+          win:perform_action({ ActivatePaneDirection = direction_keys[key] }, pane)
+        end
+      end
+    end),
+  }
+end
+
+-- move between split panes
+table.insert(config.keys, split_nav("move", "h"))
+table.insert(config.keys, split_nav("move", "j"))
+table.insert(config.keys, split_nav("move", "k"))
+table.insert(config.keys, split_nav("move", "l"))
+-- resize panes
+table.insert(config.keys, split_nav("resize", "h"))
+table.insert(config.keys, split_nav("resize", "j"))
+table.insert(config.keys, split_nav("resize", "k"))
+table.insert(config.keys, split_nav("resize", "l"))
 
 return config

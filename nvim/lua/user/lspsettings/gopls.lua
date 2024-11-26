5996e7cb166d5e199d5ae3e4d1f4e76f215f344b
issei.terada <t1l.g2.o11g@gmail.com>
2024-11-26 14:51:53 +0900

    fix

--- nvim/lua/user/lspsettings/gopls.lua
+++ nvim/lua/user/lspsettings/gopls.lua
@@ -1,8 +1,13 @@
 return {
+  cmd = {'gopls'}, -- gopls のパス
+  filetypes = {'go', 'gomod'},
+  root_dir = require('lspconfig.util').root_pattern('go.mod', '.git'),
   settings = {
     gopls = {
+      experimentalWorkspaceModule = true,
       analyses = {
         unusedparams = true,
+        unreachable = true,
       },
       staticcheck = true,
       gofumpt = true,

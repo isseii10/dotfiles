5f21b973cbf7bdaa2c4c4f9c96cd5c88be0d33c9
isseii10 <t1l.g2.o11g@gmail.com>
2024-12-01 23:06:19 +0900

    delete nohnecpain, autosession

--- nvim/lua/user/git-conflict.lua
+++ nvim/lua/user/git-conflict.lua
@@ -1,5 +1,14 @@
-return {
+local M = {
   "akinsho/git-conflict.nvim",
   version = "*",
   config = true,
 }
+
+vim.keymap.set("n", "<leader>gco", "<Plug>(git-conflict-ours)")
+vim.keymap.set("n", "<leader>gct", "<Plug>(git-conflict-theirs)")
+vim.keymap.set("n", "<leader>gcb", "<Plug>(git-conflict-both)")
+vim.keymap.set("n", "<leader>gc0", "<Plug>(git-conflict-none)")
+vim.keymap.set("n", "[x", "<Plug>(git-conflict-prev-conflict)", { desc = "prev conflict" })
+vim.keymap.set("n", "]x", "<Plug>(git-conflict-next-conflict)", { desc = "next conflict" })
+
+return M

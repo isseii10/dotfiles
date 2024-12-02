1dc62236c0907cd40a2bbddcf4ff2e0d544dc316
issei.terada <t1l.g2.o11g@gmail.com>
2024-12-02 15:00:28 +0900

    async format

--- nvim/lua/user/formatter/conform.lua
+++ nvim/lua/user/formatter/conform.lua
@@ -22,8 +22,8 @@ return {
       },
       format_on_save = {
         lsp_fallback = true,
-        async = false,
-        timeout_ms = 500,
+        async = true,
+        timeout_ms = 1000,
       },
       -- vim.keymap.set({ "n", "v" }, "<leader>lf", function()
       --   conform.format {

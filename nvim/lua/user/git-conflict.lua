2ce2c3c743696bb4823964ebbf45ee00ec1c874f
isseii10 <t1l.g2.o11g@gmail.com>
2024-12-29 05:46:33 +0900

    fix

--- nvim/lua/user/git-conflict.lua
+++ nvim/lua/user/git-conflict.lua
@@ -1,7 +1,7 @@
 local M = {
   "akinsho/git-conflict.nvim",
   version = "*",
-  config = {
+  opts = {
     default_mappings = false, -- disable buffer local mapping created by this plugin
     default_commands = true, -- disable commands created by this plugin
     disable_diagnostics = false, -- This will disable the diagnostics in a buffer whilst it is conflicted

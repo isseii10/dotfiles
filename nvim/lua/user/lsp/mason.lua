f22317f09d149371bd132b24b826220dbd26ecd9
isseii10 <t1l.g2.o11g@gmail.com>
2024-12-29 14:08:49 +0900

    fix

--- nvim/lua/user/lsp/mason.lua
+++ nvim/lua/user/lsp/mason.lua
@@ -1,63 +1,67 @@
-return {
+local M = {
   "williamboman/mason.nvim",
   dependencies = {
     "williamboman/mason-lspconfig.nvim",
     "WhoIsSethDaniel/mason-tool-installer.nvim",
   },
-  config = function()
-    -- import mason
-    local mason = require "mason"
-
-    -- import mason-lspconfig
-    local mason_lspconfig = require "mason-lspconfig"
-
-    local mason_tool_installer = require "mason-tool-installer"
-
-    -- enable mason and configure icons
-    mason.setup {
-      ui = {
-        icons = {
-          package_installed = "✓",
-          package_pending = "➜",
-          package_uninstalled = "✗",
-        },
-        border = "rounded",
-      },
-    }
-
-    mason_lspconfig.setup {
-      -- list of servers for mason to install
-      ensure_installed = {
-        "lua_ls",
-        "gopls",
-        "yamlls",
-        "ts_ls",
-        "html",
-        "cssls",
-        "tailwindcss",
-        "pyright",
-        "marksman", -- markdown
-        "sqlls",
-        "bashls",
-      },
-    }
-
-    mason_tool_installer.setup {
-      ensure_installed = {
-        -- linter
-        "pylint", -- python
-        "eslint_d",
-        "markdownlint", -- markdown
-        "protolint", -- protobuf
-
-        -- formatter
-        "prettierd", -- prettier formatter
-        "prettier", -- prettier formatter
-        "stylua", -- lua formatter
-        "isort", -- python formatter
-        "black", -- python formatter
-        "shfmt", -- sh
-      },
-    }
-  end,
+  event = "VeryLazy",
 }
+
+function M.config()
+  -- import mason
+  local mason = require "mason"
+
+  -- import mason-lspconfig
+  local mason_lspconfig = require "mason-lspconfig"
+
+  local mason_tool_installer = require "mason-tool-installer"
+
+  -- enable mason and configure icons
+  mason.setup {
+    ui = {
+      icons = {
+        package_installed = "✓",
+        package_pending = "➜",
+        package_uninstalled = "✗",
+      },
+      border = "rounded",
+    },
+  }
+
+  mason_lspconfig.setup {
+    -- list of servers for mason to install
+    ensure_installed = {
+      "lua_ls",
+      "gopls",
+      "yamlls",
+      "ts_ls",
+      "html",
+      "cssls",
+      "tailwindcss",
+      "pyright",
+      "marksman", -- markdown
+      "sqlls",
+      "bashls",
+    },
+  }
+
+  mason_tool_installer.setup {
+    ensure_installed = {
+      -- linter
+      "pylint", -- python
+      "eslint_d",
+      "markdownlint", -- markdown
+      "protolint", -- protobuf
+
+      -- formatter
+      "prettierd", -- prettier formatter
+      "prettier", -- prettier formatter
+      "stylua", -- lua formatter
+      "isort", -- python formatter
+      "black", -- python formatter
+      "shfmt", -- sh
+    },
+  }
+end
+
+return M

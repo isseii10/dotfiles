3097f071092e6545a3a2a751cfb89b3da8c28722
isseii10 <t1l.g2.o11g@gmail.com>
2025-02-18 09:20:11 +0900

    改行しても続くように修正

--- nvim/after/ftplugin/markdown.lua
+++ nvim/after/ftplugin/markdown.lua
@@ -3,3 +3,27 @@ vim.opt_local.tabstop = 2
 vim.opt_local.softtabstop = 2
 vim.opt_local.shiftwidth = 2
 vim.opt_local.expandtab = true
+
+vim.opt_local.comments = {
+  "b:*",
+  "b:-",
+  "b:+",
+  "b:1.",
+  "nb:>",
+  "b:* [ ]",
+  "b:* [x]",
+  "b:*",
+  "b:+ [ ]",
+  "b:+ [x]",
+  "b:+",
+  "b:- [ ]",
+  "b:- [x]",
+  "b:-",
+  "b:1. [ ]",
+  "b:1. [x]",
+  "b:1.",
+}
+
+-- formatoptions settings
+vim.opt_local.formatoptions:remove "c" -- Disable auto-wrap for comments
+vim.opt_local.formatoptions:append "jro" -- Join comments, auto-wrap on insert, and continue comments

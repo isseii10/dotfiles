9af8faaa624675ba12f63e6d749c0c60e61c6348
isseii10 <t1l.g2.o11g@gmail.com>
2025-03-08 11:51:18 +0900

    render-markdown

--- nvim/lua/user/extras/render-markdown.lua
+++ nvim/lua/user/extras/render-markdown.lua
@@ -1,10 +1,16 @@
 local M = {
   "MeanderingProgrammer/render-markdown.nvim",
-  opts = {
-    file_types = { "markdown", "Avante" },
-    bullet = { right_pad = 1 },
-  },
+  -- don't use latest version until this issue is resolved
+  -- https://github.com/MeanderingProgrammer/render-markdown.nvim/issues/351
+  commit = "595ac4f7e7c0eaba7bf1d8fd6ec0f6ac91c7e33b",
   ft = { "markdown", "Avante" },
+  ---@module 'render-markdown'
+  ---@type render.md.UserConfig
+  opts = {
+    bullet = {
+      right_pad = 1,
+    },
+  },
 }
 
 return M

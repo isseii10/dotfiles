1da82cd565dd262116cb2043eb6de3c3804fedb7
isseii10 <t1l.g2.o11g@gmail.com>
2025-03-26 20:15:07 +0900

    fix render-markdown

--- nvim/lua/user/extras/render-markdown.lua
+++ nvim/lua/user/extras/render-markdown.lua
@@ -1,8 +1,5 @@
 local M = {
   "MeanderingProgrammer/render-markdown.nvim",
-  -- don't use latest version until this issue is resolved
-  -- https://github.com/MeanderingProgrammer/render-markdown.nvim/issues/351
-  commit = "595ac4f7e7c0eaba7bf1d8fd6ec0f6ac91c7e33b",
   ft = { "markdown", "Avante" },
   ---@module 'render-markdown'
   ---@type render.md.UserConfig

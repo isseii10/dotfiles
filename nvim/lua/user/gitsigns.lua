6a3bab80e8ecd5340009552713e6e4cafd021e5e
isseii10 <t1l.g2.o11g@gmail.com>
2025-06-30 02:01:24 +0900

    fix

--- nvim/lua/user/gitsigns.lua
+++ nvim/lua/user/gitsigns.lua
@@ -8,14 +8,14 @@ M.config = function()
 
   local wk = require "which-key"
   wk.add {
-    { "<leader>gj", "<cmd>lua require 'gitsigns'.next_hunk({navigation_message = false})<cr>", desc = "Next Hunk" },
-    { "<leader>gk", "<cmd>lua require 'gitsigns'.prev_hunk({navigation_message = false})<cr>", desc = "Prev Hunk" },
-    { "<leader>gp", "<cmd>lua require 'gitsigns'.preview_hunk()<cr>", desc = "Preview Hunk" },
-    { "<leader>gr", "<cmd>lua require 'gitsigns'.reset_hunk()<cr>", desc = "Reset Hunk" },
-    { "<leader>gl", "<cmd>lua require 'gitsigns'.blame_line()<cr>", desc = "Blame" },
-    { "<leader>gR", "<cmd>lua require 'gitsigns'.reset_buffer()<cr>", desc = "Reset Buffer" },
-    { "<leader>gs", "<cmd>lua require 'gitsigns'.stage_hunk()<cr>", desc = "Stage Hunk" },
-    { "<leader>gu", "<cmd>lua require 'gitsigns'.undo_stage_hunk()<cr>", desc = "Undo Stage Hunk" },
+    { "<leader>gj", require("gitsigns").next_hunk { navigation_message = false }, desc = "Next Hunk" },
+    { "<leader>gk", require("gitsigns").prev_hunk { navigation_message = false }, desc = "Prev Hunk" },
+    { "<leader>gp", require("gitsigns").preview_hunk(), desc = "Preview Hunk" },
+    { "<leader>gr", require("gitsigns").reset_hunk(), desc = "Reset Hunk" },
+    { "<leader>gl", require("gitsigns").blame_line(), desc = "Blame" },
+    { "<leader>gR", require("gitsigns").reset_buffer(), desc = "Reset Buffer" },
+    { "<leader>gs", require("gitsigns").stage_hunk(), desc = "Stage Hunk" },
+    { "<leader>gu", require("gitsigns").undo_stage_hunk(), desc = "Undo Stage Hunk" },
     { "<leader>gd", "<cmd>Gitsigns diffthis HEAD<cr>", desc = "Git Diff" },
   }
 
--- nvim/lua/user/mini-surround.lua
+++ nvim/lua/user/mini-surround.lua
@@ -8,16 +8,16 @@ function M.config()
   require("mini.surround").setup {
     -- Module mappings. Use `''` (empty string) to disable one.
     mappings = {
-      add = "gsa",            -- Add surrounding in Normal and Vsual modes
-      delete = "gsd",         -- Delete surroundng
-      find = "gsf",           -- Find surrounding (to the right)
-      find_left = "gsF",      -- Find surrounding (to the left)
-      highlight = "gsh",      -- Highlight surrounding
-      replace = "gsr",        -- Replace surrounding
+      add = "gsa", -- Add surrounding in Normal and Vsual modes
+      delete = "gsd", -- Delete surroundng
+      find = "gsf", -- Find surrounding (to the right)
+      find_left = "gsF", -- Find surrounding (to the left)
+      highlight = "gsh", -- Highlight surrounding
+      replace = "gsr", -- Replace surrounding
       update_n_lines = "gsn", -- Update `n_lines`
 
-      suffix_last = "l",      -- Suffix to search with "prev" method
-      suffix_next = "n",      -- Suffix to search with "next" method
+      suffix_last = "l", -- Suffix to search with "prev" method
+      suffix_next = "n", -- Suffix to search with "next" method
     },
   }
 end

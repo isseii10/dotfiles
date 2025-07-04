9a7fbc03c1a80500c8f86efa6a7d1f8d09cb4cba
isseii10 <t1l.g2.o11g@gmail.com>
2025-07-05 06:32:26 +0900

    indent-blanklineからhlchunkにかえる

--- nvim/lua/user/hlchunk.lua
+++ nvim/lua/user/hlchunk.lua
@@ -6,8 +6,36 @@ local M = {
 function M.config()
   require("hlchunk").setup {
     chunk = {
-      enable=true,
-    }
+      enable = true,
+      priority = 15,
+      style = {
+        { fg = "#c795ba" },
+        { fg = "#c21f30" },
+      },
+      use_treesitter = true,
+      chars = {
+        horizontal_line = "─",
+        vertical_line = "│",
+        left_top = "╭",
+        left_bottom = "╰",
+        right_arrow = ">",
+      },
+      textobject = "",
+      max_file_size = 1024 * 1024,
+      error_sign = true,
+      -- animation related
+      duration = 200,
+      delay = 200,
+    },
+    indent = {
+      enable = true,
+    },
+    line_num = {
+      enable = false,
+      style = "#c795ba",
+      priority = 10,
+      use_treesitter = false,
+    },
   }
 end
 

363ea9902f3bb34270aec74c51888330f6c2efb0
isseii10 <t1l.g2.o11g@gmail.com>
2025-08-31 12:48:24 +0900

    test commit

--- nvim/lua/user/extras/octo.lua
+++ nvim/lua/user/extras/octo.lua
@@ -3,11 +3,10 @@ return {
   dependancies = {
     "nvim-lua/plenary.nvim",
     "nvim-telescope/telescope.nvim",
-    -- OR 'ibhagwan/fzf-lua',
-    -- OR 'folke/snacks.nvim',
     "nvim-tree/nvim-web-devicons",
   },
   config = function()
     require("octo").setup()
   end,
 }
+-- comment

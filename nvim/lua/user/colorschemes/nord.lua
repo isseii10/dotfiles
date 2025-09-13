346158a0d309d935f2625bfd22e214666ea0d597
isseii10 <t1l.g2.o11g@gmail.com>
2025-09-13 18:02:10 +0900

    nordic

--- nvim/lua/user/colorschemes/nord.lua
+++ nvim/lua/user/colorschemes/nord.lua
@@ -1,11 +1,8 @@
-local M = {
+return {
   "shaunsingh/nord.nvim",
   lazy = false, -- make sure we load this during startup if it is your main colorscheme
   priority = 1000, -- make sure to load this before all the other start plugins
+  config = function()
+    vim.cmd.colorscheme "nord"
+  end,
 }
-
-function M.config()
-  vim.cmd.colorscheme "nord"
-end
-
-return M
--- nvim/lua/user/colorschemes/onenord.lua
+++ nvim/lua/user/colorschemes/onenord.lua
@@ -1,12 +1,8 @@
-local M = {
+return {
   "rmehri01/onenord.nvim",
   lazy = false, -- make sure we load this during startup if it is your main colorscheme
   priority = 1000, -- make sure to load this before all the other start plugins
+  config = function()
+    vim.cmd.colorscheme "onenord"
+  end,
 }
-
-function M.config()
-  vim.cmd.colorscheme "onenord"
-end
-
-return M
-

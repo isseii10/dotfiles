9376269d25b8b43b678ff322379551857098eef0
isseii10 <t1l.g2.o11g@gmail.com>
2025-09-14 03:47:51 +0900

    registers border

--- nvim/lua/user/extras/registers.lua
+++ nvim/lua/user/extras/registers.lua
@@ -1,12 +1,25 @@
 local M = {
   "tversteeg/registers.nvim",
   cmd = "Registers",
-  config = true,
   keys = {
     { '"', mode = { "n", "v" } },
     { "<C-R>", mode = "i" },
   },
   name = "registers",
+  config = function()
+    require("registers").setup {
+      window = {
+        -- The window can't be wider than 100 characters
+        max_width = 100,
+        -- Show a small highlight in the sign column for the line the cursor is on
+        highlight_cursorline = true,
+        -- Don't draw a border around the registers window
+        border = "rounded",
+        -- Apply a tiny bit of transparency to the the window, letting some characters behind it bleed through
+        transparency = 10,
+      },
+    }
+  end,
 }
 
 return M

6b10fed9d11db0ac5450453804a4302372df6f99
isseii10 <t1l.g2.o11g@gmail.com>
2026-01-19 22:04:48 +0900

    flashをmにしてsurroundをsにする

--- nvim/lua/user/mini-surround.lua
+++ nvim/lua/user/mini-surround.lua
@@ -8,13 +8,12 @@ function M.config()
   require("mini.surround").setup {
     -- Module mappings. Use `''` (empty string) to disable one.
     mappings = {
-      add = "gsa", -- Add surrounding in Normal and Vsual modes
-      delete = "gsd", -- Delete surroundng
-      find = "gsf", -- Find surrounding (to the right)
-      find_left = "gsF", -- Find surrounding (to the left)
-      highlight = "gsh", -- Highlight surrounding
-      replace = "gsr", -- Replace surrounding
-      update_n_lines = "gsn", -- Update `n_lines`
+      add = "sa", -- Add surrounding in Normal and Visual modes
+      delete = "sd", -- Delete urrounding
+      find = "sf", -- Find surrounding (to the right)
+      find_left = "sF", -- Find surrounding (to the left)
+      highlight = "sh", -- Highlight surrounding
+      replace = "sr", -- Replace surrounding
 
       suffix_last = "l", -- Suffix to search with "prev" method
       suffix_next = "n", -- Suffix to search with "next" method

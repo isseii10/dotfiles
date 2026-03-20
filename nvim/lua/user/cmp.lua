db06f0e3ad38cefbdad7544784d28d3c554b396b
isseii10 <t1l.g2.o11g@gmail.com>
2026-03-20 10:56:31 +0900

    fix(nvim/cmp): replace deprecated nvim_buf_get_option with vim.bo

--- nvim/lua/user/cmp.lua
+++ nvim/lua/user/cmp.lua
@@ -67,7 +67,7 @@ function M.config()
   local icons = require "user.icons"
 
   local has_words_before = function()
-    if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
+    if vim.bo.buftype == "prompt" then
       return false
     end
     local line, col = unpack(vim.api.nvim_win_get_cursor(0))

cf016c9df1166526bd3d6fa9a597752974d1f161
isseii10 <t1l.g2.o11g@gmail.com>
2026-04-05 02:42:34 +0900

    feat(nix): add git config via Home Manager xdg symlink

--- home.nix
+++ home.nix
@@ -1,6 +1,9 @@
 { config, pkgs, ... }:
 
 {
+  imports = [
+    ./git/git.nix
+  ];
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
   home.username = "isseiterada";

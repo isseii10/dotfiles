e70a534a8a824bfa3d61be6c2198f1da255398da
isseii10 <t1l.g2.o11g@gmail.com>
2026-04-12 00:51:44 +0900

    refactor(git): manage config as template copied on first setup

--- git/default.nix
+++ git/default.nix
@@ -1,8 +1,14 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
   home.packages = [ pkgs.git ];
 
-  xdg.configFile."git/config".source =
-    config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/git/config";
+  # テンプレートを初回のみコピー（既存ファイルは上書きしない）
+  home.activation.copyGitConfig = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    if [ ! -f "${config.home.homeDirectory}/.config/git/config" ]; then
+      $DRY_RUN_CMD mkdir -p "${config.home.homeDirectory}/.config/git"
+      $DRY_RUN_CMD cp "${config.home.homeDirectory}/dotfiles/git/config" \
+        "${config.home.homeDirectory}/.config/git/config"
+    fi
+  '';
 }
